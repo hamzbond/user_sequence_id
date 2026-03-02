9557569ffc6d018b17164f84bae7a6bdfe05aa97
hamzbond <hamzbond@gmail.com>
2026-03-02 07:33:35 +0000

    chore: sync module structure and static files with branch 11.0

---
+++ user_sequence_id/__manifest__.py
@@ -0,0 +1,33 @@
+{
+    'name': 'User ID in Sequence Legend',
+    'version': '1.0',
+    'category': 'Extra Tools',
+    'summary': 'Add current User ID to sequence prefix or suffix using %(uid)s',
+    'description': """
+This module extends the Odoo sequence functionality to allow using the current user's ID as a dynamic placeholder in prefixes and suffixes.
+
+Key Features:
+-------------
+* Adds support for `%(uid)s` in any sequence definition.
+* Returns the ID of the user who is currently performing the action.
+* Compatible with standard Odoo sequence legends (year, month, day, etc.).
+* Lightweight and safe: falls back to default Odoo behavior if placeholder is not used.
+
+Usage:
+------
+1. Go to Settings > Technical > Sequences & Identifiers > Sequences.
+2. Edit a sequence (e.g., Sales Order).
+3. Add `%(uid)s` to the Prefix or Suffix field.
+4. The placeholder will be replaced by the numeric User ID in the generated number.
+    """,
+    'author': 'hamzbond',
+    'website': 'https://hamzbond.github.io',
+    'license': 'LGPL-3',
+    'support': 'hamzbond@gmail.com',
+    'images': ['static/description/banner.png'],
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
